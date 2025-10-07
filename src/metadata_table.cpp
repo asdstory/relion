8f8230d9df40d3ff1bb4ae10493f3fcd8e4867fd
Tongyi Dou <47798921+asdstory@users.noreply.github.com>
2025-10-07 13:20:53 -0400

    Update metadata_table.cpp

--- src/metadata_table.cpp
+++ src/metadata_table.cpp
@@ -2209,6 +2209,7 @@ MetaDataTable removeDuplicatedParticles(MetaDataTable &MDin, EMDLabel mic_label,
 	if (fn_removed != "")
 		MDremoved.write(fn_removed);
 
+	long n_final = 0;
 	n_final = MDin.numberOfObjects() - n_removed;
 	std::cout << "Removed " << n_removed << " duplicated objects from " << MDin.numberOfObjects() << " objects." << std::endl;
 	std::cout << "We now have " << n_final << " objects in the final star file. " << std::endl;
