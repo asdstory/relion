1137823dde1577393258687c0985f2a07a7ffdee
Tongyi Dou <47798921+asdstory@users.noreply.github.com>
2025-11-07 18:01:09 -0500

    Update displayer.cpp

--- src/displayer.cpp
+++ src/displayer.cpp
@@ -182,11 +182,11 @@ void DisplayBox::setData(MultidimArray<RFLOAT> &img, MetaDataContainer *MDCin, i
 
 	// For micrographs use relion-scaling to avoid bias in down-sampled positions
 	// For multi-image viewers, do not use this scaling as it is slower...
-	if (!do_relion_scale && ABS(scale - 1.0) > 0.01)
+	if (do_relion_scale && ABS(scale - 1.0) > 0.01)
 		selfScaleToSize(img, xsize_data, ysize_data);
 
 	// Use the same nearest-neighbor algorithm as in the copy function of Fl_Image...
-	if (ABS(scale - 1.0) > 0.01 && do_relion_scale)
+	if (ABS(scale - 1.0) > 0.01 && !do_relion_scale)
 	{
 		int xmod   = XSIZE(img) % xsize_data;
 		int xstep  = XSIZE(img) / xsize_data;
