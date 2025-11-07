9698af0ff88d3178cb3fa5d41ed4727d9cfa52a5
Tongyi Dou <47798921+asdstory@users.noreply.github.com>
2025-11-07 17:05:27 -0500

    Update displayer.cpp

--- src/displayer.cpp
+++ src/displayer.cpp
@@ -188,7 +188,8 @@ void DisplayBox::setData(MultidimArray<RFLOAT> &img, MetaDataContainer *MDCin, i
 	// Use the same nearest-neighbor algorithm as in the copy function of Fl_Image...
 	if (ABS(scale - 1.0) > 0.01 && !do_relion_scale)
 	{
-		int xmod   = XSIZE(img) % xsize_data;
+		selfScaleToSize(img, xsize_data, ysize_data);
+/*		int xmod   = XSIZE(img) % xsize_data;
 		int xstep  = XSIZE(img) / xsize_data;
 		int ymod   = YSIZE(img) % ysize_data;
 		int ystep  = YSIZE(img) / ysize_data;
@@ -246,6 +247,7 @@ void DisplayBox::setData(MultidimArray<RFLOAT> &img, MetaDataContainer *MDCin, i
 				}
 			}
 		}
+*/
 	}
 	else
 	{
